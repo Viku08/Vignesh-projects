479cbc6e70903a49780156a933e68e3f47eaa645
Viku08 <90371392+Viku08@users.noreply.github.com>
2023-07-31 16:54:03 +0530

    Add files via upload

---
+++ VAC/Task-1/bulb.js
@@ -0,0 +1,6 @@
+function on(){
+    document.getElementById('image').src='https://media.istockphoto.com/id/1413658359/vector/light-bulb-icon-energy-and-thinking-symbol.jpg?s=612x612&w=0&k=20&c=LEsVhJYWKeiYNYzlTuU_903LvxFSSDQUA8poCg54xeg='
+}
+function off(){
+    document.getElementById('image').src='https://media.istockphoto.com/id/1150776990/vector/%C3%B0%C3%B0%C2%B5%C3%B0%C3%B1%C3%B0%C3%B1%C3%B0%C3%B0%C3%B1%C3%B0-2.jpg?s=612x612&w=0&k=20&c=J8qy9IxZ42caK1yDwu8Y9UNhX7fEhHVC-8A_WRXzKTA='
+}
---
+++ VAC/Task-2/react.js
@@ -0,0 +1,15 @@
+
+function add(){
+    let a=parseInt(document.getElementById('a').value);
+    let b=parseInt(document.getElementById('b').value);
+    let  c=a+b;
+    document.getElementById("sol").innerHTML="Solution after addition is:"+c;
+
+}
+function sub(){
+    let a=parseInt(document.getElementById('a').value);
+    let b=parseInt(document.getElementById('b').value);
+    let  c=a-b;
+    document.getElementById("sol2").innerHTML="Solution after addition is:"+c;
+
+}
